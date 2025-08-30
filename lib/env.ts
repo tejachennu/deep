61331ed7d8d4fb0246502f8536c78c713872c2e1
Teja Chennu <50200888+tejachennu@users.noreply.github.com>
2025-08-30 12:14:41 +0530

    Update env.ts

--- lib/env.ts
+++ lib/env.ts
@@ -13,32 +13,20 @@ const envSchema = z.object({
 
 // For client components
 export const clientEnv = {
-  NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_Zl5mVSbnDgUKF8' ,
+  NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_RBRzmAQOwt8WAl' ,
 }
 
-// For server components
-// export const env = {
-//   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID!,
-//   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET!,
-//   MYSQL_HOST: process.env.MYSQL_HOST!,
-//   MYSQL_PORT: process.env.MYSQL_PORT!,
-//   MYSQL_USER: process.env.MYSQL_USER!,
-//   MYSQL_PASSWORD: process.env.MYSQL_PASSWORD!,
-//   MYSQL_DATABASE: process.env.MYSQL_DATABASE!,
-//   NEXT_PUBLIC_RAZORPAY_KEY_ID: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-// }
 
 export const env = {
-  RAZORPAY_KEY_ID: 'rzp_live_Zl5mVSbnDgUKF8',
-  RAZORPAY_KEY_SECRET: 'xYHpqE9nhJw7xmJVfCSdyegv',
+  RAZORPAY_KEY_ID: 'rzp_live_RBRzmAQOwt8WAl',
+  RAZORPAY_KEY_SECRET: 'thdjWneoVPgFlhaIUfZExz2k',
   MYSQL_HOST: '194.163.45.105',
   MYSQL_PORT: `3306`,
   MYSQL_USER: 'Deep',
   MYSQL_PASSWORD: 'Deep@123',
   MYSQL_DATABASE: 'deep_payments',
-  NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_Zl5mVSbnDgUKF8',
+  NEXT_PUBLIC_RAZORPAY_KEY_ID: 'rzp_live_RBRzmAQOwt8WAl',
 }
-
 // Validate environment variables
 try {
   envSchema.parse(env)
